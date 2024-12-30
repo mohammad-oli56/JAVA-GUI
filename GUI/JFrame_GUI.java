bae23b66b444f24a0ade8369701510b0304ff849
Mohammad oli56 <olimohammad286@gmai.com>
2024-12-30 14:15:23 +0600

    The Java GUI frame

---
+++ GUI/JFrame_GUI.java
@@ -0,0 +1,10 @@
+package GUI;
+
+public class JFrame_GUI {
+    public static void main(String[] args) {
+
+        new MYframe();
+        
+    }
+    
+}
---
+++ GUI/MYframe.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class MYframe extends JFrame{
+
+    MYframe(){
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(420,500);
+        this.setResizable(true);
+        this.getContentPane().setBackground(Color.CYAN);
+    }
+
+}
